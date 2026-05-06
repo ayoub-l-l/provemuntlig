b815b271604ee80ed344c93e9bd3af405ce04469
ayoub-l-l <ayela006@osloskolen.no>
2026-05-06 14:53:13 +0200

    fikset cors error

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ let app = express();
 
 app.use(express.json());
 
+// CORS FIX
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
   next();
 });
 
-// hent data fra fil
 let quotes = JSON.parse(fs.readFileSync("quotes.json"));
 
 // hent alle
@@ -17,13 +19,13 @@ app.get("/quotes", function(req, res) {
   res.json(quotes);
 });
 
-// random quote
+// tilfeldig
 app.get("/quotes/random", function(req, res) {
   let randomIndex = Math.floor(Math.random() * quotes.length);
   res.json(quotes[randomIndex]);
 });
 
-// legg til ny quote
+// legg til
 app.post("/quotes", function(req, res) {
   let newQuote = {
     id: quotes.length + 1,
@@ -32,12 +34,11 @@ app.post("/quotes", function(req, res) {
 
   quotes.push(newQuote);
 
-  // lagre tilbake til fil
   fs.writeFileSync("quotes.json", JSON.stringify(quotes, null, 2));
 
   res.json(newQuote);
 });
 
 app.listen(3000, function() {
-  console.log("http://localhost:3000/quotes/random");
+  console.log("http://localhost:3000/quotes");
 });
